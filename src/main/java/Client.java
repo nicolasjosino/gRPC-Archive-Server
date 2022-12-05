d3a82f0ec57caab61c79f93ba722ca7f7cbf80f1
Nicolas Josino <nicolasjosino@gmail.com>
2022-12-05 11:24:34 -0300

    Client implemented

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,33 +1,143 @@
+import com.google.protobuf.ByteString;
 import generated.*;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+
+import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) {
         ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8080)
                 .usePlaintext()
                 .build();
-
         ArchiveManagerServiceGrpc.ArchiveManagerServiceBlockingStub stub
                 = ArchiveManagerServiceGrpc.newBlockingStub(channel);
 
-        ChangeDirectoryDownResponse directoryDown
-                = stub.changeDirectoryDown(ChangeDirectoryDownRequest.newBuilder()
-                .setName("archives")
-                .build());
+        menu();
+        Scanner name = new Scanner(System.in);
 
-        System.out.println(directoryDown);
+        while (true) {
+            System.out.print("Choose a procedure to call: ");
+            int selection = new Scanner(System.in).nextInt();
 
-        ListFilesResponse listFilesResponse = stub.listFiles(ListFilesRequest.newBuilder().build());
+            switch (selection) {
+                case 0: System.exit(0);
 
-        System.out.println(listFilesResponse);
+                case 1:
+                    callListFiles(stub);
+                    break;
 
-        GetArchiveResponse getArchive = stub.getArchive(GetArchiveRequest.newBuilder()
-                                                        .setName("textfile.txt")
-                                                        .build());
+                case 2:
+                    System.out.print("Write the desired file name: ");
+                    String getFile = name.nextLine();
+                    callGetArchive(stub, getFile);
+                    break;
 
-        System.out.println(getArchive);
+                case 3:
+                    System.out.print("Write the file name to be deleted: ");
+                    String deleteFile = name.nextLine();
+                    callDeleteArchive(stub, deleteFile);
+                    break;
 
-         channel.shutdown();
+                case 4:
+                    System.out.print("Write the directory to be listed: ");
+                    String dir = name.nextLine();
+                    callListChildren(stub, dir);
+                    break;
+
+                case 5:
+                    System.out.print("Write the file name to be sent: ");
+                    String fileName = name.nextLine();
+                    System.out.println("Write the file's content: ");
+                    String content = new Scanner(System.in).nextLine();
+                    callSendArchive(stub, fileName, ByteString.copyFromUtf8(content));
+                    break;
+
+                case 6:
+                    callPwd(stub);
+                    break;
+
+                case 7:
+                    callChangeDirUp(stub);
+                    break;
+
+                case 8:
+                    System.out.print("Write the folder name to access: ");
+                    String dirDown = name.nextLine();
+                    callChangeDirDown(stub, dirDown);
+                    break;
+
+                default:
+                    System.out.println("Choose a valid option!");
+                    break;
+            }
+        }
+
     }
+
+    public static void menu() {
+        System.out.println("Available procedures");
+        System.out.println("0 - Quit");
+        System.out.println("1 - List files");
+        System.out.println("2 - Get archive");
+        System.out.println("3 - Delete archive");
+        System.out.println("4 - List children");
+        System.out.println("5 - Send archive");
+        System.out.println("6 - Current directory (pwd)");
+        System.out.println("7 - Change directory up");
+        System.out.println("8 - Change directory down");
+    }
+
+    public static void callListFiles(ArchiveManagerServiceGrpc.ArchiveManagerServiceBlockingStub stub) {
+        ListFilesResponse response = stub.listFiles(ListFilesRequest.newBuilder().build());
+        System.out.println(response);
+    }
+
+    public static void callGetArchive(ArchiveManagerServiceGrpc.ArchiveManagerServiceBlockingStub stub,
+                                      String name) {
+        GetArchiveRequest request = GetArchiveRequest.newBuilder().setName(name).build();
+        GetArchiveResponse response = stub.getArchive(request);
+        System.out.println(response);
+    }
+
+    public static void callDeleteArchive(ArchiveManagerServiceGrpc.ArchiveManagerServiceBlockingStub stub,
+                                         String name) {
+        DeleteArchiveRequest request = DeleteArchiveRequest.newBuilder().setName(name).build();
+        DeleteArchiveResponse response = stub.deleteArchive(request);
+        System.out.println(response);
+    }
+
+    public static void callListChildren(ArchiveManagerServiceGrpc.ArchiveManagerServiceBlockingStub stub,
+                                        String name) {
+        ListChildrenRequest request = ListChildrenRequest.newBuilder().setName(name).build();
+        ListChildrenResponse response = stub.listChildren(request);
+        System.out.println(response);
+    }
+
+    public static void callSendArchive(ArchiveManagerServiceGrpc.ArchiveManagerServiceBlockingStub stub,
+                                       String name, ByteString contents) {
+        SendArchiveRequest request = SendArchiveRequest.newBuilder().setName(name).setContents(contents).build();
+        SendArchiveResponse response = stub.sendArchive(request);
+        System.out.println(response);
+    }
+
+    public static void callPwd(ArchiveManagerServiceGrpc.ArchiveManagerServiceBlockingStub stub) {
+        PwdRequest request = PwdRequest.newBuilder().build();
+        PwdResponse response = stub.pwd(request);
+        System.out.println(response);
+    }
+
+    public static void callChangeDirUp(ArchiveManagerServiceGrpc.ArchiveManagerServiceBlockingStub stub) {
+        ChangeDirectoryUpRequest request = ChangeDirectoryUpRequest.newBuilder().build();
+        ChangeDirectoryUpResponse response = stub.changeDirectoryUp(request);
+        System.out.println(response);
+    }
+
+    public static void callChangeDirDown(ArchiveManagerServiceGrpc.ArchiveManagerServiceBlockingStub stub,
+                                     String name) {
+        ChangeDirectoryDownRequest request = ChangeDirectoryDownRequest.newBuilder().setName(name).build();
+        ChangeDirectoryDownResponse response = stub.changeDirectoryDown(request);
+        System.out.println(response);
+    }
+
 }
